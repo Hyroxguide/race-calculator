4f41888fd413f5bab805b1f5ef5c9c2b16cf687f
Hyroxguide <hyroxguide@gmail.com>
2026-03-09 19:36:19 +0900

    Update drum.js

--- js/drum.js
+++ js/drum.js
@@ -110,7 +110,7 @@ class DrumRoller {
         // Mouse wheel
         this.container.addEventListener('wheel', (e) => {
             e.preventDefault();
-            const delta = e.deltaY > 0 ? -1 : 1;
+            const delta = e.deltaY > 0 ? -4 : 4;
             const newVal = Math.max(0, Math.min(this.max, this.value - delta));
             this.setValue(newVal);
             if (this.onChange) this.onChange(this.value);
